2e78273ae9b692da48b069e58b7b4754fc0618cc
Noushadozi <noushadozi333@gmail.com>
2023-08-18 19:58:39 +0600

    email and pass given

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById("submit-button").addEventListener("click", function () {
   let email = document.getElementById("email").value;
   let pass = document.getElementById("pass").value;
-  if (email == "noushadozi333@gmail.com" && pass == 1234) {
+  if (email == "3@gmail.com" && pass == 1234) {
     window.location.href = "./bank.html";
   } else {
     alert("Invalid User");
